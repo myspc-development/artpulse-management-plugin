d673e5eed412dbc177c53e484faec7f70fe242ed
myspchosting <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>
2025-06-16 11:01:58 +0000

    Add stripe_webhook_secret field to SettingsPage

--- src/Core/SettingsPage.php
+++ src/Core/SettingsPage.php
@@ -5,18 +5,18 @@ class SettingsPage
 {
     public static function register()
     {
-        add_action('admin_menu',   [self::class, 'addMenu']);
-        add_action('admin_init',   [self::class, 'registerSettings']);
+        add_action('admin_menu', [ self::class, 'addMenu' ]);
+        add_action('admin_init', [ self::class, 'registerSettings' ]);
     }
 
     public static function addMenu()
     {
         add_options_page(
-            __('ArtPulse Settings','artpulse'),
-            __('ArtPulse','artpulse'),
+            __('ArtPulse Settings', 'artpulse'),
+            __('ArtPulse', 'artpulse'),
             'manage_options',
             'artpulse-settings',
-            [self::class, 'render']
+            [ self::class, 'render' ]
         );
     }
 
@@ -26,32 +26,33 @@ class SettingsPage
 
         add_settings_section(
             'ap_membership_section',
-            __('Membership Settings','artpulse'),
+            __('Membership Settings', 'artpulse'),
             '__return_false',
             'artpulse-settings'
         );
 
         $fields = [
-            'basic_fee'       => 'Basic Member Fee ($)',
-            'pro_fee'         => 'Pro Artist Fee ($)',
-            'org_fee'         => 'Organization Fee ($)',
-            'currency'        => 'Currency (ISO)',
-            'stripe_enabled'  => 'Enable Stripe Integration',
-            'stripe_pub_key'  => 'Stripe Publishable Key',
-            'stripe_secret'   => 'Stripe Secret Key',
-            'stripe_test'     => 'Stripe Test Mode',
-            'woo_enabled'     => 'Enable WooCommerce Integration',
-            'notify_fee'      => 'Email Notification on Fee Change',
+            'basic_fee'              => 'Basic Member Fee ($)',
+            'pro_fee'                => 'Pro Artist Fee ($)',
+            'org_fee'                => 'Organization Fee ($)',
+            'currency'               => 'Currency (ISO)',
+            'stripe_enabled'         => 'Enable Stripe Integration',
+            'stripe_pub_key'         => 'Stripe Publishable Key',
+            'stripe_secret'          => 'Stripe Secret Key',
+            'stripe_webhook_secret'  => 'Stripe Webhook Signing Secret',
+            'stripe_test'            => 'Stripe Test Mode',
+            'woo_enabled'            => 'Enable WooCommerce Integration',
+            'notify_fee'             => 'Email Notification on Fee Change',
         ];
 
         foreach ($fields as $name => $label) {
             add_settings_field(
                 $name,
-                __($label,'artpulse'),
-                [self::class, 'renderField'],
+                __($label, 'artpulse'),
+                [ self::class, 'renderField' ],
                 'artpulse-settings',
                 'ap_membership_section',
-                ['label_for' => $name]
+                [ 'label_for' => $name ]
             );
         }
     }
@@ -60,10 +61,9 @@ class SettingsPage
     {
         $opts = get_option('artpulse_settings', []);
         $val  = $opts[$args['label_for']] ?? '';
-        $type = in_array($args['label_for'], ['stripe_enabled','stripe_test','woo_enabled','notify_fee'])
-                ? 'checkbox' : 'text';
+        $checkboxFields = ['stripe_enabled', 'stripe_test', 'woo_enabled', 'notify_fee'];
 
-        if ($type === 'checkbox') {
+        if (in_array($args['label_for'], $checkboxFields, true)) {
             printf(
                 '<input type="checkbox" id="%1$s" name="artpulse_settings[%1$s]" value="1" %2$s />',
                 esc_attr($args['label_for']),
@@ -80,7 +80,7 @@ class SettingsPage
 
     public static function render()
     {
-        echo '<div class="wrap"><h1>'.__('ArtPulse Settings','artpulse').'</h1>';
+        echo '<div class="wrap"><h1>' . __('ArtPulse Settings', 'artpulse') . '</h1>';
         echo '<form method="post" action="options.php">';
         settings_fields('artpulse_settings_group');
         do_settings_sections('artpulse-settings');
